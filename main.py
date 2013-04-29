5a8d52f2b71d22ab08fa389bdbdc80ac969d4e52
quake0day <quake0day@gmail.com>
2013-04-28 21:28:59 -0400

    add several funcs to deal with save score and file operation

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ import re
 DB_URL = 'localhost'
 DB_PORT = 27017
 DB_NAME = 'eco-record'
-
+RESULT = 'result.txt'
+global result_set
 
 class MongoDB:
     def __init__(self):
         self.client = MongoClient(DB_URL, DB_PORT)
         self.db = self.client[DB_NAME]
 
-    #def insert(username, eco, result):
-       # post = {"name": username,   
+    #def insert(playername, eco, result):
+       # post = {"name": playername,   
       #          }
 #data = {}
 
@@ -40,30 +41,55 @@ def parse_res(res_string):
     return res
 
 
-def update_eco_res(dataset, eco, res):
+def cal_eco_res(dataset, eco, res):
     # parse eco
     m = re.search('(?<=[A-E])\w+', eco)
-
+    index = int(m.group(0))
     if 'A' in eco:
-        index = int(m.group(0))
         if(index >= 0 and index < 100):
             dataset[index] = dataset[index] + res
     elif 'B' in eco:
-        m = re.search('(?<=[A-E])\w+', eco)
-        print int(m.group(0))
+        if(index >= 0 and index < 100):
+            index = index + 100
+            dataset[index] = dataset[index] + res
     elif 'C' in eco:
-        m = re.search('(?<=[A-E])\w+', eco)
-        print int(m.group(0))
+            index = index + 200
+            dataset[index] = dataset[index] + res
     elif 'D' in eco:
-        m = re.search('(?<=[A-E])\w+', eco)
-        print int(m.group(0))
+        if(index >= 0 and index < 100):
+            index = index + 300
+            dataset[index] = dataset[index] + res
+    elif 'E' in eco:
+        if(index >= 0 and index < 100):
+            index = index + 400
+            dataset[index] = dataset[index] + res
     else:
         pass
+    return dataset
 
 
-def save_eco(username, eco_res):
-    if username not in data:
-        data[username] = eco
+def save_eco(playername, eco_res):
+    result_set = ["", ""]
+    result_set[0] = playername
+    result_set[1] = eco_res
+    #print result_set
+    fr = open(RESULT, 'a+')
+    for item in result_set:
+        print item
+        print>>fr, item
+    fr.close()
+    #number_of_lines = len(fr.readlines())
+
+    #if playername not in data:
+        #data[playername] = eco
+
+
+def find_existing_record(playername):
+    fr = open(RESULT, 'r+')
+    for line in fr.readlines():
+        if line.find(playername) >= 0:
+            return True
+    return False
 
 
 # parse PGN file and output ECO code for each game
@@ -71,17 +97,21 @@ def parse_pgn(filename):
     path = os.path.join(filename)
     pgn_text = open(path).read()
     #pgn_game = pgn.PGNGame()
-
     games = pgn.loads(pgn_text)
     for game in games:
         #print game.moves
         #moves = game.moves[0]+" "+game.moves[1]+" "+game.moves[2]+" "+game.moves[3]
-        print game.white
-        print game.result
-        print parse_res(game.result)
-        print eco.from_moves(game.moves)[0][0]
+        #print game.result
+        res = parse_res(game.result)
+        eco_ = eco.from_moves(game.moves)[0][0]
+        dataset = create_eco_res()
+        playername = game.white
+        eco_res = cal_eco_res(dataset, eco_, res)
+        #save_eco(playername, eco_res)
+        print find_existing_record(playername)
+
     #print games[0].moves
-    print pgn.dumps(games[0])
+    #print pgn.dumps(games[0])
 
 
 parse_pgn("./data/RR1600in2006Exp.pgn")
